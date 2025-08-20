fae3878012d36084aaa7b5882695271cca2e9ba3
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-08-20 20:48:06 +0530

    make the routes for this site

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,13 +2,14 @@ import express from "express";
 import {
   register,
   login,
-  logout,
   updateProfile,
-} from "../controllers/user.controller";
+} from "../controllers/user.controller.js";
+import isAuthenticated from "../middlewares/isAuthenticated.js";
 
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("/logout").post(logout);
-router.route("/profile/update").post(updateProfile);
+router.route("/profile/update").post(isAuthenticated, updateProfile);
+
+export default router;
