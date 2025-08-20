66f8d6f86d8ac5b8ce2baf9c94f0ab12a165f611
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-08-20 20:30:28 +0530

    make the user controller for the site like [login, register, logout, updateProfile]

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,171 @@
+import { User } from "../models/user.model.js";
+import bcrpyt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { use } from "react";
+
+export const register = async (req, res) => {
+  try {
+    const { fullName, email, phoneNumber, password, role } = req.body;
+
+    if (!fullName || !email || !phoneNumber || !password || !role) {
+      return res.status(400).json({
+        message: "All fields are required, Some fields are missing",
+        success: false,
+      });
+    }
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        message: "User already exists with this Email",
+        success: false,
+      });
+    }
+
+    const passwordHash = await bcrpyt.hash(password, 10);
+
+    await User.create({
+      fullname,
+      email,
+      phoneNumber,
+      password: passwordHash,
+      role,
+    });
+
+    return res.status(201).json({
+      message: "Account created successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+
+    if (!email || !password || !role) {
+      return res.status(400).json({
+        message: "All fields are required, Some fields are missing",
+        success: false,
+      });
+    }
+
+    let user = await User.findOne({ email });
+    const isPasswordMatch = await bcrpyt.compare(password, user.password);
+
+    if (!user || !isPasswordMatch) {
+      return res.status(400).json({
+        message: "Incorrect Email or password",
+        success: false,
+      });
+    }
+
+    // Check user role
+    if (role !== user.role) {
+      return res.status(400).json({
+        message: "Account doesn't exist with current role",
+        success: false,
+      });
+    }
+
+    const tokenData = {
+      userId: user._id,
+    };
+
+    const token = await jwt.sign(tokenData, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    user = {
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      role: user.role,
+      profile: user.profile,
+    };
+
+    return res
+      .status(200)
+      .cookie("token", token, {
+        maxAge: 24 * 60 * 60 * 1000,
+        httpsOnly: true,
+        sameSite: "strict",
+      })
+      .json({
+        message: `Welcome back ${user.fullName}`,
+        user,
+        success: true,
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+      message: "Logged out successfully",
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { fullName, email, phoneNumber, bio, skills } = req.body;
+    const file = req.file;
+
+    if (!fullName || !email || !phoneNumber || !bio || !skills) {
+      return res.status(400).json({
+        message: "All fields are required, Some fields are missing",
+        success: false,
+      });
+    }
+
+    // TODO: Resume File setup
+
+    const skillsArray = skills.split(",");
+    const userId = req.id; // come from the middleware of the auth
+
+    let user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+        success: false,
+      });
+    }
+
+    user.fullname = fullName;
+    user.email = email;
+    user.phoneNumber = phoneNumber;
+    user.profile.bio = bio;
+    user.profile.skills = skillsArray;
+
+    // TODO: Resume
+
+    await user.save();
+
+    user = {
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      role: user.role,
+      profile: user.profile,
+    };
+
+    return res.status(200).json({
+      message: "Profile updated successfully",
+      user,
+      success: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
